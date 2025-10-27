c88bcd88c5becaea08c7e5bcb5d0457fc99be754
digilaravelproject <digilaravelproject2025@gmail.com>
2025-10-27 11:17:40 +0530

    Updated files

--- resources/views/subscription/expired.blade.php
+++ resources/views/subscription/expired.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container text-center py-5">
       <h1 class="text-danger">🚫 Access Denied</h1>
-      <p>Your subscription expired. Allowed year: {{ env('SUBSCRIPTION_YEAR') }}.</p>
+      <p>Your subscription expired. Allowed year till: {{ env('SUBSCRIPTION_YEAR') }}.</p>
       <a href="{{ url('/') }}" class="btn btn-primary mt-3">Renew Subscription</a>
   </div>
 @endsection
